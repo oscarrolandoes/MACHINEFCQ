627af6b28ac3183dbaa03d55e0ed2d2b91827777
OSCAR ROLANDO <116234228+oscarrolandoes@users.noreply.github.com>
2022-10-20 08:39:33 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("MI APLICACIÓN")
+st.sidebar.header("ESTA APLICACIÓN ES SOLO UN DEMO")
+st.sidebar.image("http://fcq.uach.mx/images/institucionales/Escudos/Dorado.png")
+boton = st.button("globos")
+if boton:
+  st.balloon()
