dfb219fef02ce1e4970f0457dccc34e819e7be8b
OSCAR ROLANDO <116234228+oscarrolandoes@users.noreply.github.com>
2022-10-20 08:59:59 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import streamlit as st
-st.title("MI APLICACIÓN")
-st.sidebar.header("ESTA APLICACIÓN ES SOLO UN DEMO")
-st.sidebar.image("http://fcq.uach.mx/images/institucionales/Escudos/Dorado.png")
-boton = st.button("globos")
-if boton:
-  st.balloons()
-#comentario#
+from img_classification import teachable_machine_classification
+import keras
+from PIL import Image, ImageOps
+import numpy as np
+
+st.title("BRAIN TUMORS IMAGE RECOGNIZER")
+
+uploaded_file = st.file_uploader("Carga una imagen ...", type=["jpg","jpeg"])
+if uploaded_file is not None:
+  image = Image.open(uploaded_file)
+  st.image(image)
+  label = teachable_machine_classification(image, 'keras_model.h5')
+  if label == 0:
+    st.write("YES TUMORAL FORMATIONS")
+
+  else:
+    st.write("NO, TUMORAL FORMATIONS")
